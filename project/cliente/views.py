83fe2667852fe3fbbbc958541614d2bd162dac4c
Marcos Emmanuel Vellani <marcosvellani1@gmail.com>
2023-09-27 20:01:51 -0300

    Update-views, models, index

--- project/cliente/views.py
+++ project/cliente/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.shortcuts import render
+from . import models
+
 
 def index(request):
-    contexto = {"name" : "Cliente"}
-    return render(request, "cliente/index.html", contexto)
+    cliente = models.Cliente.objects.all()
+
+    return render(request, "cliente/index.html", {"cliente":cliente})

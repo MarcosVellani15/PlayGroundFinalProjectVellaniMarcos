9d522d66e5cf35b300b5243b9b18790cfe101911
Marcos Emmanuel Vellani <marcosvellani1@gmail.com>
2023-09-27 21:40:37 -0300

    update-como_filtrar

--- project/cliente/models.py
+++ project/cliente/models.py
@@ -2,19 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Pais(models.Model):
-    name = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
 
     def __str__(self) -> str:
-        return self.name 
+        return self.nombre 
     
 class clientes(models.Model):
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    birth = models.DateField(null=True)
-    country_origin_id = models.ForeignKey(Pais, on_delete=models.SET_NULL, null=True, blank=True)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    nacimiento = models.DateField(null=True)
+    pais_origen_id = models.ForeignKey(Pais, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self) -> str:
-        return f"{self.name} {self.surname}"
-    
-class productos(models.Model):
-    pass
+        return f"{self.nombre} {self.apellido}"
